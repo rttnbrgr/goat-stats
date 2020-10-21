8fd4ae50ac151aa05ca7a8cf1b022d88b4e8393d
Nathan Rothenberger <nroth10@gmail.com>
2020-10-21 17:20:50 -0500

    cleanup homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,9 @@ import Card from "./../components/Card";
 import tw from "twin.macro";
 import players from "../../static/data";
 
-// const Home = () => (
-//   <Layout>
-//     <div tw="px-4 py-4 bg-gray-200 uppercase font-bold text-base leading-none">
-//       Goat Stats
-//     </div>
-//     {players.map(({ name, imgSrc, stats, id }) => (
-//       <Card name={name} imageSrc={imgSrc} stats={stats} key={id} />
-//     ))}
-//   </Layout>
-// );
-
 const Wrapper = tw.main`px-0`;
 
-function Home2() {
+function Home() {
   const [activeCard, setActiveCard] = useState(null);
 
   const cardClick = cardIndex => {
@@ -47,59 +36,4 @@ function Home2() {
   );
 }
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeCard: null,
-    };
-    // this.handleClick = this.handleClick.bind(this);
-  }
-
-  // handleClick = () => {
-  //   console.log("click");
-  //   this.setState((state, props) => ({
-  //     state: !state.active,
-  //   }));
-  // };
-
-  handleClick(cardIndex) {
-    console.log("click me now", cardIndex);
-    console.log("this", this);
-    this.setState(state => ({
-      activeCard: state.activeCard === cardIndex ? null : cardIndex,
-    }));
-    console.log("after");
-  }
-
-  render() {
-    return (
-      <Layout>
-        {/* Header */}
-        <div tw="px-4 py-4 bg-gray-200 uppercase font-bold text-base leading-none">
-          Goat Stats: | {this.state.activeCard}
-        </div>
-        <Wrapper>
-          {/* {players.slice(0, 1).map(({ name, imgSrc, stats, id }) => ( */}
-          {players.map(({ name, imgSrc, stats, id }, index) => {
-            return (
-              <Card
-                name={name}
-                imageSrc={imgSrc}
-                stats={stats}
-                key={id}
-                active={this.state.activeCard === index}
-                onClick={e => this.handleClick(index)}
-              />
-            );
-          })}
-        </Wrapper>
-      </Layout>
-    );
-  }
-}
-
-export default Home2;
-
-// look, function, feel
-// storymapping/storyboarding
+export default Home;
