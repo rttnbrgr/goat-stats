37f78ff3634f1efa964fb4b11ac79afdb6d49ad8
Nathan Rothenberger <nroth10@gmail.com>
2020-10-22 11:15:41 -0500

    add new statistics master object. back fill player stats to match

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,18 @@ import players from "../../static/data";
 
 const Wrapper = tw.main`px-0`;
 
+const statisticMaster = [
+  { id: "STAT-PTS", name: "PTS", visible: true },
+  { id: "STAT-AST", name: "AST", visible: true },
+  { id: "STAT-REB", name: "REB", visible: true },
+  { id: "STAT-4", name: "Stat 4", visible: true },
+  { id: "STAT-5", name: "Stat 5", visible: true },
+  { id: "STAT-6", name: "Stat 6", visible: true },
+  { id: "STAT-7", name: "Stat 7", visible: true },
+  { id: "STAT-8", name: "Stat 8", visible: true },
+  { id: "STAT-9", name: "Stat 9", visible: true },
+];
+
 function Home() {
   const [activeCard, setActiveCard] = useState(null);
 
--- static/data.js
+++ static/data.js
@@ -4,9 +4,15 @@ export const playerMike = {
   imgSrc:
     "https://www.basketball-reference.com/req/202010061/images/players/jordami01.jpg",
   stats: [
-    { name: "Pts", value: 30.1 },
-    { name: "Reb", value: 6.2 },
-    { name: "Ast", value: 5.3 },
+    { name: "foo", id: "STAT-PTS", value: 30.1 },
+    { name: "foo", id: "STAT-AST", value: 6.2 },
+    { name: "foo", id: "STAT-REB", value: 5.3 },
+    { name: "foo", id: "STAT-4", value: 0.0 },
+    { name: "foo", id: "STAT-5", value: 0.0 },
+    { name: "foo", id: "STAT-6", value: 0.0 },
+    { name: "foo", id: "STAT-7", value: 0.0 },
+    { name: "foo", id: "STAT-8", value: 0.0 },
+    { name: "foo", id: "STAT-9", value: 0.0 },
   ],
 };
 
@@ -17,9 +23,15 @@ export const playerKobe = {
     "https://www.basketball-reference.com/req/202010061/images/players/bryanko01.jpg",
 
   stats: [
-    { name: "Pts", value: 25.0 },
-    { name: "Reb", value: 5.2 },
-    { name: "Ast", value: 4.7 },
+    { name: "foo", id: "STAT-PTS", value: 25.0 },
+    { name: "foo", id: "STAT-AST", value: 5.2 },
+    { name: "foo", id: "STAT-REB", value: 4.7 },
+    { name: "foo", id: "STAT-4", value: 0.0 },
+    { name: "foo", id: "STAT-5", value: 0.0 },
+    { name: "foo", id: "STAT-6", value: 0.0 },
+    { name: "foo", id: "STAT-7", value: 0.0 },
+    { name: "foo", id: "STAT-8", value: 0.0 },
+    { name: "foo", id: "STAT-9", value: 0.0 },
   ],
 };
 
@@ -29,9 +41,15 @@ export const playerBron = {
   imgSrc:
     "https://www.basketball-reference.com/req/202010061/images/players/jamesle01.jpg",
   stats: [
-    { name: "Pts", value: 27.1 },
-    { name: "Reb", value: 7.4 },
-    { name: "Ast", value: 7.4 },
+    { name: "foo", id: "STAT-PTS", value: 27.1 },
+    { name: "foo", id: "STAT-AST", value: 7.4 },
+    { name: "foo", id: "STAT-REB", value: 7.4 },
+    { name: "foo", id: "STAT-4", value: 0.0 },
+    { name: "foo", id: "STAT-5", value: 0.0 },
+    { name: "foo", id: "STAT-6", value: 0.0 },
+    { name: "foo", id: "STAT-7", value: 0.0 },
+    { name: "foo", id: "STAT-8", value: 0.0 },
+    { name: "foo", id: "STAT-9", value: 0.0 },
   ],
 };
 
